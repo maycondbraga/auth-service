ae3d82c056880b239b2b565d13fb5b4788cce70a
maycondbraga <maycondbraga.s@gmail.com>
2024-12-02 23:00:52 -0500

    feat: add pre-authorization role

--- src/main/java/com/md/authservice/entities/User.java
+++ src/main/java/com/md/authservice/entities/User.java
@@ -4,10 +4,12 @@ import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Table(name = "users")
@@ -41,7 +43,9 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + role.getName().name());
+
+        return List.of(authority);
     }
 
     @Override
